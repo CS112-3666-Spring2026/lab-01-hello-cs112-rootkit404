ad3e3ef717a3c38a426f933cf1bea46f9b0525c7
Nathan Tshishimbi <149440288+nathanltish@users.noreply.github.com>
2026-02-01 23:56:12 +0000

    lab submission 1

--- Main.java
+++ Main.java
@@ -1,5 +1,35 @@
+/**
+ * CS112 - Lab 01
+ * 
+ *  @author Nathan Tshishimbi <W7404862@student.miracosta.edu>
+ *  @version 1.0
+ */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome to CS112!");
+
+        System.out.println("Do I like reading?\n" + Main.checksHobby("reading"));
+        Main.printHobbies();
+
+        Student student1 = new Student("W7404001", null, 2, 0);
+
+        student1.setMajor("Computer Science");
+        student1.setGpa(3.7);
+
+        System.out.println("Here is my student information: "+ student1);
+    }
+
+    public static void printHobbies() {
+        System.out.println("Some of my hobbies include the gym, basketball, running, reading books and manga, pottery, and watching anime, movies, and shows.");
+    }
+
+    public static String checksHobby(String hobby) {
+        String myHobbies = "Some of my hobbies include the gym, basketball, running, reading books and manga, pottery, and watching anime, movies, and shows.";
+
+        if (myHobbies.contains(hobby)) {
+            return "Yes! You like " + hobby + "!";
+        } else {
+            return "No unfortunately you don't :(";
+        }
     }
 }
